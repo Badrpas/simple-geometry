9579764321c1b36fe07363a412f0618de1aac3c6
Vadim Samarin <dezivert@gmail.com>
2017-03-11 19:21:09 +0200

    Added immutable tests

--- test/index.js
+++ test/index.js
@@ -38,8 +38,43 @@ describe('Vector', function () {
       let original = new Vector({x: 2, y: 3}); 
       test
         .object(new Vector(original))
-        .contains({x: 2, y: 3})
-        .isNotEqualTo(original);
+        .contains({x: 2, y: 3});
+    });
+  });
+
+  describe("Immutable", function() {
+    let original;
+
+    beforeEach(function () {
+      original = new Vector({x: 2, y: 3});
+    });
+
+    it("Creation from other Vector", function() {
+      test.assert(new Vector(original) !== original);
+    });
+
+    it("normalize()", function() {
+      test.assert(original.normalize() !== original);
+    });
+
+    it("invert()", function() {
+      test.assert(original.invert() !== original);
+    });
+
+    it("setLength()", function() {
+      test.assert(original.setLength(123) !== original);
+    });
+
+    it("add()", function() {
+      test.assert(original.add(new Vector(3,2)) !== original);
+    });
+    
+    it("sub()", function() {
+      test.assert(original.sub(new Vector(3,2)) !== original);
+    });
+    
+    it("interpolate()", function() {
+      test.assert(original.interpolate(new Vector(3,2), 0.5) !== original);
     });
   });
 });
